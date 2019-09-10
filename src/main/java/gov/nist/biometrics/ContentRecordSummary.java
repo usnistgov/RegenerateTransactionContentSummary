0eb8434e29b63c73e3b06b60b590c1bdd5a1f216
Andrew McCaffrey <andrew.mccaffrey@nist.gov>
2019-09-10 16:46:57 -0400

    Fleshed out ContentRecordSummary and some boilerplate code added

--- src/main/java/gov/nist/biometrics/ContentRecordSummary.java
+++ src/main/java/gov/nist/biometrics/ContentRecordSummary.java
@@ -1,11 +1,19 @@
 
 package gov.nist.biometrics;
 
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+
+
 /**
  *
  * @author mccaffrey
  */
 public class ContentRecordSummary {
+
     
     private String imageReferenceID = null;
     private String recordCategoryCode = null;
@@ -36,6 +44,76 @@ public class ContentRecordSummary {
      */
     public void setRecordCategoryCode(String recordCategoryCode) {
         this.recordCategoryCode = recordCategoryCode;
+    }
+    public static boolean isContentRecordSummary(Node node) {
+
+        String name = node.getNodeName();
+        if(name == null) return false;
+        if(name.endsWith("PackageDescriptiveTextRecord") ||
+                name.endsWith("PackageHighResolutionGrayscaleImageRecord") ||
+                name.endsWith("PackageUserDefinedImageRecord") ||
+                name.endsWith("PackageSignatureImageRecord") ||
+                name.endsWith("PackageMinutiaeRecord") ||
+                name.endsWith("PackageFacialAndSMTImageRecord") ||
+                name.endsWith("PackageVoiceDataRecord") ||
+                name.endsWith("PackageForensicDentalDataRecord") ||
+                name.endsWith("PackageLatentImageRecord") ||
+                name.endsWith("PackageFingerprintImageRecord") ||
+                name.endsWith("PackagePalmprintImageRecord") ||
+                name.endsWith("PackageUserDefinedTestingImageRecord") ||
+                name.endsWith("PackageIrisImageRecord") ||
+                name.endsWith("PackageDNARecord") ||
+                name.endsWith("PackagePlantarImageRecord") ||
+                name.endsWith("PackageSourceRepresentationRecord") ||
+                name.endsWith("PackageAssociatedContextRecord") ||
+                name.endsWith("PackageNonPhotographicImageryRecord") ||
+                name.endsWith("PackageInformationAssuranceRecord") ||
+                name.endsWith("PackageCBEFFBiometricDataRecord") )
+            return true;
+
+        return false;
+    }
+    
+    static ContentRecordSummary convertRecordToCRS(Element record) {
+        ContentRecordSummary crs = new ContentRecordSummary();
+        
+        String recordName = record.getTagName();
+
+        if(recordName.endsWith("PackageDescriptiveTextRecord")) crs.setRecordCategoryCode("02"); else if
+            (recordName.endsWith("PackageHighResolutionGrayscaleImageRecord")) crs.setRecordCategoryCode("04"); else if
+            (recordName.endsWith("PackageUserDefinedImageRecord")) crs.setRecordCategoryCode("07"); else if
+            (recordName.endsWith("PackageSignatureImageRecord")) crs.setRecordCategoryCode("08"); else if
+            (recordName.endsWith("PackageMinutiaeRecord")) crs.setRecordCategoryCode("09"); else if
+            (recordName.endsWith("PackageFacialAndSMTImageRecord")) crs.setRecordCategoryCode("10"); else if
+            (recordName.endsWith("PackageVoiceDataRecord")) crs.setRecordCategoryCode("11"); else if                                                                                
+            (recordName.endsWith("PackageForensicDentalDataRecord")) crs.setRecordCategoryCode("12"); else if
+            (recordName.endsWith("PackageLatentImageRecord")) crs.setRecordCategoryCode("13"); else if
+            (recordName.endsWith("PackageFingerprintImageRecord")) crs.setRecordCategoryCode("14"); else if
+            (recordName.endsWith("PackagePalmprintImageRecord")) crs.setRecordCategoryCode("15"); else if
+            (recordName.endsWith("UserDefinedTestingImageRecord")) crs.setRecordCategoryCode("16"); else if
+            (recordName.endsWith("PackageIrisImageRecord")) crs.setRecordCategoryCode("17"); else if
+            (recordName.endsWith("PackageDNARecord")) crs.setRecordCategoryCode("18"); else if
+            (recordName.endsWith("PackagePlantarImageRecord>")) crs.setRecordCategoryCode("19"); else if
+            (recordName.endsWith("PackageSourceRepresentationRecord")) crs.setRecordCategoryCode("20"); else if
+            (recordName.endsWith("PackageAssociatedContextRecord")) crs.setRecordCategoryCode("21"); else if
+            (recordName.endsWith("PackageNonPhotographicImageryRecord")) crs.setRecordCategoryCode("22"); else if
+            (recordName.endsWith("PackageInformationAssuranceRecord")) crs.setRecordCategoryCode("98"); else if
+            (recordName.endsWith("PackageCBEFFBiometricDataRecord")) crs.setRecordCategoryCode("99");            
+        
+
+        NodeList children = record.getChildNodes();
+        for(int i = 0; i < children.getLength(); i++) {
+            if(children.item(i).getNodeName().endsWith("ImageReferenceID")) {
+                Node id = children.item(i);
+                System.out.println("text content" + id.getTextContent());
+                Element idElement = (Element) id;            
+                String idValue = idElement.getNodeValue();
+                crs.setImageReferenceID(idValue);
+                    
+            }
+        }
+
+        return crs;
     }
     
 }
--- src/main/java/gov/nist/biometrics/RegenerateTransactionContentSummary.java
+++ src/main/java/gov/nist/biometrics/RegenerateTransactionContentSummary.java
@@ -1,10 +1,45 @@
-
 package gov.nist.biometrics;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  *
  * @author mccaffrey
  */
 public class RegenerateTransactionContentSummary {
-    
+
+    public static void run() throws ParserConfigurationException, SAXException, IOException {
+
+        String filename = "/home/mccaffrey/biometrics/401/xml/AllFields401.xml";
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(new File(filename));
+
+        ArrayList<ContentRecordSummary> contentRecordSummarys = new ArrayList<>();
+        
+        Element nistBiometricInformationExchangePackage = doc.getDocumentElement();                
+        NodeList firstChildren = nistBiometricInformationExchangePackage.getChildNodes();
+        for(int i = 0; i < firstChildren.getLength(); i++) {
+            if(ContentRecordSummary.isContentRecordSummary(firstChildren.item(i))) {
+               Element record = (Element) firstChildren.item(i);
+                contentRecordSummarys.add(ContentRecordSummary.convertRecordToCRS(record));
+            }
+        }
+        
+        
+     //   /itl:NISTBiometricInformationExchangePackage/itl:PackageInformationRecord[1]/itl:Transaction[1]/itl:TransactionContentSummary[1]
+        
+    }
+    public static final void main(String [] args) throws ParserConfigurationException, IOException, SAXException {
+        RegenerateTransactionContentSummary.run();
+    }
 }
